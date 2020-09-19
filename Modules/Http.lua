c1e7ee89aaa48353f54303ff8edc41c65ee0741f
bacionBOI <58794828+bacionBOI@users.noreply.github.com>
2020-09-19 15:04:43 -0400

    Create Http.lua

---
+++ Modules/Http.lua
@@ -0,0 +1,27 @@
+local config = require(script.Parent.Config)
+
+return {
+	delete = function(path,body)
+		local Delete = game.HttpService:RequestAsync({
+			Url = config.GlitchURL..'/'..path,
+			Method = "DELETE",
+			Headers = {
+				["Content-Type"] = Enum.HttpContentType.ApplicationJson
+			},
+			Body = body
+		})
+		local Body = game.HttpService:JSONDecode(Delete.Body)
+		if Body.Sucess then
+			return true
+		end
+	end;
+	post = function(path,body)
+		local POST = game.HttpService:PostAsync(config.GlitchURL..'/'..path,body)
+		local body = game.HttpService:JSONDecode(POST.Body)
+		if body.Sucess then
+			return true
+		else
+			error(body.Errors)
+		end
+	end
+}
